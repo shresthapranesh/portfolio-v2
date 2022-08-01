b79aac81292113b256573965deb02f05447517e4
Pranesh Shrestha <shresthapranesh@Praneshs-MacBook-Pro.local>
2022-07-31 22:18:10 -0700

    updated cloudbuild.yaml

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import styled from '@emotion/styled'
 
 import {Stack} from './Stack/Stack'
+import { SocialButton } from "./SocialButton";
 
 const Layout = styled('div')({
   display:'flex',
@@ -32,25 +33,11 @@ const Page = (props:PageProps) => {
 
         <Stack justifyContent="center" alignItems="center" direction="row">
                 <h1>Links</h1>
-                <a href="https://www.github.com/shresthapranesh">
-                  <Image height={32} width={32} alt="Github" src="https://img.icons8.com/color/32/000000/github--v1.png" />
-                </a>
-              
-                <a href="https://www.linkedin.com/in/praneshshrestha">
-                  <Image height={32} width={32} alt="LinkedIn" src="https://img.icons8.com/color/32/000000/linkedin.png" />
-                </a>
-              
-                <a href="https://www.twitter.com/shresthapranesh">
-                  <Image height={32} width={32} alt="Twitter" src="https://img.icons8.com/color/32/000000/twitter-squared.png" />
-                </a>
-              
-                <a href="https://www.facebook.com/pranesh.shrestha">
-                  <Image height={32} width={32} alt="Facebook" src="https://img.icons8.com/color/32/000000/facebook-new.png" />
-                </a>
-              
-                <a href="https://www.instagram.com/praneshshrestha13/">
-                  <Image height={32} width={32} alt="Instagram" src="https://img.icons8.com/color/32/000000/instagram-new.png" />
-                </a>
+                <SocialButton link="https://www.github.com/shresthapranesh" img="https://img.icons8.com/color/32/000000/github--v1.png" />
+                <SocialButton link="https://www.linkedin.com/in/praneshshrestha" img="https://img.icons8.com/color/32/000000/linkedin.png" />
+                <SocialButton link="https://www.twitter.com/shresthapranesh" img="https://img.icons8.com/color/32/000000/twitter-squared.png" />
+                <SocialButton link="https://www.facebook.com/pranesh.shrestha" img="https://img.icons8.com/color/32/000000/facebook-new.png" />
+                <SocialButton link="https://www.instagram.com/praneshshrestha13/" img="https://img.icons8.com/color/32/000000/instagram-new.png" />
         </Stack>
       </Layout>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,8 @@ import {Stack} from '../components'
 import Layout from "../components/Layout";
 import { useMediaQuery } from "../hooks";
 import {Button} from 'antd'
-import { Typography } from "antd";
+import { Typography } from "antd"
+import {css} from '@emotion/react';
 const {Title, Paragraph, Text} = Typography
 
 const MyImage = () => {
@@ -20,33 +21,27 @@ const MyImage = () => {
 
 const Index = () => {
   const smallDevice = useMediaQuery("(max-width: 468px)")
-  const handleClick = () => {
-    fetch('/api/getResume',{
-      method: "GET",
-    }).then(response => response.blob()).then( blob => {
-      const url = URL.createObjectURL(blob)
-      window.open(url)
-    }).catch((e) => console.log(e))
-  }
 
   return (
     <Layout>
-      <Stack justifyContent="center" flexGrow={1} sx={{paddingTop:20}} direction={smallDevice?"column":"row"}>
-        <MyImage />
+      <Stack 
+        direction={smallDevice?"column":"row"}
+      >
+          <MyImage />
         
-        <Stack justifyContent='center' alignContent="center" sx={{paddingLeft:100}}>
-          <Typography>
-            <Paragraph>
-              Hello, there <br />
-              I am
-              <Title level={2}>Pranesh Shrestha</Title>
-            </Paragraph>
-            <Paragraph>
-              I am a full time Software Engineer.
-              <br /> Welcome to my Portfolio Website.
-            </Paragraph>
-          </Typography>
-        </Stack>
+          <Stack justifyContent='center' alignContent="center" sx={{paddingLeft:100}}>
+            <Typography>
+              <Paragraph>
+                Hello, there <br />
+                I am
+                <Title level={2}>Pranesh Shrestha</Title>
+              </Paragraph>
+              <Paragraph>
+                I am a full time Software Engineer.
+                <br /> Welcome to my Portfolio Website.
+              </Paragraph>
+            </Typography>
+          </Stack>
       </Stack>
     </Layout>
   );
