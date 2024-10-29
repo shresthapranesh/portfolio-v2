f3aab17a3a6870268f8f09b8e6b7c580ab0c5c2c
Pranesh Shrestha <praneshshrestha10@gmail.com>
2024-10-29 12:59:13 -0700

    next v15

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ['en-US'],
     defaultLocale: "en-US",
   },
   images: {
-    domains: ['img.icons8.com'],
+    remotePatterns: [
+      {
+        hostname: 'img.icons8.com',
+      }
+    ],
   }
 };
 
