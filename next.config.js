cddeced6da8a899e477328096130727d84ef9642
Pranesh Shrestha <praneshshrestha10@gmail.com>
2025-12-27 13:31:40 -0800

    update react and next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: "en-US",
-  },
   images: {
     remotePatterns: [
       {
